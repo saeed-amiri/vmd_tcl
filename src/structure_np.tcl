8e985755eb759a0293aeb2042b6c9ebd9e486e0e
Saeed Amiri <samiri@uni-bremen.de>
2024-03-31 16:30:11 +0200

    visualize and save all

--- src/structure_np.tcl
+++ src/structure_np.tcl
@@ -5,11 +5,11 @@ set env(VMDOPTIXWRITEALPHA) 1
 mol delete all
 
 # Define a procedure to load a molecule
-proc load_molecule {filename} {
+proc load_molecule {style filename} {
     # Delete all molecules
     mol delete all
     # Load the molecule
-    set mol [mol load gro $filename]
+    set mol [mol load $style $filename]
 }
 
 # Define a procedure to delete all representations
@@ -29,6 +29,17 @@ proc render_image {filename} {
     render TachyonLOptiXInternal $filename
 }
 
+proc reset_view_and_display {} {
+    display resetview
+    display projection Orthographic
+    axes location Off
+    display depthcue on
+
+    light 1 on
+    light 2 on
+    light 3 off
+    light 4 off
+}
 # Define a procedure with parameters
 # Define a procedure with parameters
 proc visualize_residues {repIndex selection radius} {
@@ -67,15 +78,7 @@ proc visualize_system_face_cut {} {
     visualize_residues 8 "resname D10 and z > 90 and (x+y+z)<320" 2.0
 
     # Reset the view
-    display resetview
-    display projection Orthographic
-    axes location Off
-    display depthcue on
-
-    light 1 on
-    light 2 on
-    light 3 off
-    light 4 off
+    reset_view_and_display
 
     rotate z by -65
     rotate y by 0
@@ -92,16 +95,8 @@ proc visualize_system_face_cut {} {
 
 proc visualize_np_core {} {
     visualize_residues 0 "resname COR" 2.0
-    
-    display resetview
-    display projection Orthographic
-    axes location Off
-    display depthcue on
 
-    light 1 on
-    light 2 on
-    light 3 off
-    light 4 off
+    reset_view_and_display    
 
     scale by 1.6
     translate by 0 0 0
@@ -114,20 +109,12 @@ proc visualize_np {} {
     delete_all_reps
     visualize_residues 0 "resname COR" 1.5
     visualize_residues 1 "resname APT and not name N" 1.5
-    mol modcolor 1 top "ColorID 2"
+    # mol modcolor 1 top "ColorID 2"
     # Selecting the NH3 atoms of the aptes
     visualize_residues 2 "name N" 2.0
     mol modcolor 2 top "ColorID 0"
 
-    display resetview
-    display projection Orthographic
-    axes location Off
-    display depthcue on
-
-    light 1 on
-    light 2 on
-    light 3 off
-    light 4 off
+    reset_view_and_display
 
     scale by 1.6
     translate by 0 0 0
@@ -136,7 +123,56 @@ proc visualize_np {} {
     pbc box -width 6 -color black
 }
 
-load_molecule "npt.gro"
+proc visualize_decane {} {
+    delete_all_reps
+    visualize_residues 0 "resname D10" 2.1
+    reset_view_and_display
+    rotate z by 70
+    rotate y by 30
+    rotate x by 15
+}
+
+proc visualize_water {} {
+    delete_all_reps
+    visualize_residues 0 "resname SOL" 2.0
+    reset_view_and_display
+}
+
+proc visualize_cl {} {
+    delete_all_reps
+    visualize_residues 0 "resname CLA" 2.0
+    reset_view_and_display
+     mol modcolor 0 top "ColorID 7"
+}
+
+proc visualize_na {} {
+    delete_all_reps
+    visualize_residues 0 "resname POT" 2.0
+    reset_view_and_display
+    mol modcolor 0 top "ColorID 30"
+}
+
+proc visualize_apt {} {
+    delete_all_reps
+    visualize_residues 0 "resname APT and not name N" 2.3
+    visualize_residues 1 "name N" 2.
+    mol modcolor 1 top "ColorID 0"
+    reset_view_and_display
+    rotate z by 0
+    rotate y by 0
+    rotate x by 20
+}
+
+proc visualize_oda {} {
+    delete_all_reps
+    visualize_residues 0 "resname ODA" 2.3
+    visualize_residues 1 "name N" 2.3
+    mol modcolor 1 top "ColorID 3"
+    reset_view_and_display
+    rotate x by 20
+}
+
+load_molecule gro "npt.gro"
 delete_all_reps
 visualize_system_face_cut
 render_image "structure_100Oda.png"
@@ -144,16 +180,58 @@ render_image "structure_100Oda.png"
 delete_all_reps
 mol delete all
 
-load_molecule "npt.gro"
+load_molecule gro "npt.gro"
 delete_all_reps
 visualize_np_core
 render_image "structure_core.png"
 
 mol delete all
 
-load_molecule "npt.gro"
+load_molecule gro "npt.gro"
 delete_all_reps
 visualize_np
 render_image "structure_np.png"
+
+mol delete all
+
+load_molecule pdb "D10.pdb"
+delete_all_reps
+visualize_decane
+render_image "decane.png"
+
+mol delete all
+
+load_molecule pdb "water.pdb"
+delete_all_reps
+visualize_water
+render_image "water.png"
+
+mol delete all
+
+load_molecule pdb "aptes.pdb"
+delete_all_reps
+visualize_apt
+render_image "aptes.png"
+
+mol delete all
+
+load_molecule pdb "CLA.pdb"
+delete_all_reps
+visualize_cl
+render_image "cl.png"
+
+mol delete all
+
+load_molecule pdb "POT.pdb"
+delete_all_reps
+visualize_na
+render_image "na.png"
+
+mol delete all
+
+load_molecule pdb "ODAp.pdb"
+delete_all_reps
+visualize_oda
+render_image "oda.png"
 
 exit
