20679dd17f788f55f27c57b2c0409bbe33bb7de2
Saeed Amiri <samiri@uni-bremen.de>
2024-04-29 11:11:44 +0200

    update colors

--- src/structure_np.tcl
+++ src/structure_np.tcl
@@ -2,54 +2,9 @@
 package require pbctools
 set env(VMDOPTIXWRITEALPHA) 1
 
+source /scratch/saeed/MyScripts/vmd_tcl/src/display_procs.tcl
+
 mol delete all
-
-# Define a procedure to load a molecule
-proc load_molecule {style filename} {
-    # Delete all molecules
-    mol delete all
-    # Load the molecule
-    set mol [mol load $style $filename]
-}
-
-# Define a procedure to delete all representations
-proc delete_all_reps {} {
-    # Get the number of representations
-    set numreps [molinfo top get numreps]
-
-    # Delete all representations
-    for {set i 0} {$i < $numreps} {incr i} {
-        mol delrep 0 top
-    }
-}
-
-# Define a procedure to render an image
-proc render_image {filename} {
-    display resize 4800 4800
-    render TachyonLOptiXInternal $filename
-}
-
-proc reset_view_and_display {} {
-    display resetview
-    display projection Orthographic
-    axes location Off
-    display depthcue on
-
-    light 1 on
-    light 2 on
-    light 3 off
-    light 4 off
-}
-# Define a procedure with parameters
-# Define a procedure with parameters
-proc visualize_residues {repIndex selection radius} {
-    # Add a new representation with the specified selection
-    mol addrep top
-    mol modselect $repIndex top $selection
-    mol modstyle $repIndex top "CPK $radius 0.0 30.0 12.0"
-    mol modmaterial $repIndex top Glassy
-}
-
 
 proc visualize_system_face_cut {} {
     # Selecting water molecules
@@ -60,7 +15,7 @@ proc visualize_system_face_cut {} {
     visualize_residues 2 "resname APT and not name N" 2.0
     mol modcolor 2 top "ColorID 2"
     # Selecting the NH3 atoms of the aptes
-    visualize_residues 3 "name N" 3.0
+    visualize_residues 3 {name N} 3.0
     mol modcolor 3 top "ColorID 0"
     # Selecting the CL ions
     visualize_residues 4 "resname CLA" 3.0
@@ -69,13 +24,18 @@ proc visualize_system_face_cut {} {
     visualize_residues 5 "resname POT" 4.0
     mol modcolor 5 top "ColorID 30"
     # Selecting the ODA surfactants with the NH2 atoms hidden
-    visualize_residues 6 "resname ODN and z>90 and not name NH2" 2.0
-    mol modcolor 6 top "ColorID 4"
+    visualize_residues 6 {name "HA.*"} 2.0
+    mol modcolor 6 top "ColorID 8"
+    visualize_residues 7 "resname ODN and z>90 and not name NH2" 2.0
+    mol modcolor 7 top "ColorID 2"
     # Selecting the NH2 atoms of the ODA surfactants
-    visualize_residues 7 "name NH2 and z > 90" 4.0
-    mol modcolor 7 top "ColorID 3"
+    visualize_residues 8 "name NH2 and z > 90" 4.0
+    mol modcolor 8 top "ColorID 3"
     # Selecting oil molecules
-    visualize_residues 8 "resname D10 and z > 90 and (x+y+z)<320" 2.0
+    visualize_residues 9 "resname D10 and z > 90 and (x+y+z)<320" 2.0
+    mol modcolor 9 top "ColorID 4"
+    # visualize_residues 10 "resname APT and 'HA.*'" 2.0
+    # mol modcolor 10 top "ColorID 2"
 
     # Reset the view
     reset_view_and_display
@@ -108,11 +68,15 @@ proc visualize_np_core {} {
 proc visualize_np {} {
     delete_all_reps
     visualize_residues 0 "resname COR" 1.5
-    visualize_residues 1 "resname APT and not name N" 1.5
+    # visualize_residues 1 "resname APT and not name N" 1.5
+    visualize_residues 1 {resname APT and name "C.*" and not name N} 1.5
+    mol modcolor 1 top "ColorID 2"
+    visualize_residues 2 {resname ODA and name "H.*" and not name N} 2.3
+    mol modcolor 2 top "ColorID 8"
     # mol modcolor 1 top "ColorID 2"
     # Selecting the NH3 atoms of the aptes
-    visualize_residues 2 "name N" 2.0
-    mol modcolor 2 top "ColorID 0"
+    visualize_residues 3 "name N" 2.0
+    mol modcolor 3 top "ColorID 0"
 
     reset_view_and_display
 
@@ -125,7 +89,10 @@ proc visualize_np {} {
 
 proc visualize_decane {} {
     delete_all_reps
-    visualize_residues 0 "resname D10" 2.1
+    visualize_residues 0 {resname D10 and name "C.*"} 2.1
+    mol modcolor 0 top "ColorID 2"
+    visualize_residues 1 {resname D10 and name "H.*"} 2.1
+    mol modcolor 1 top "ColorID 8"
     reset_view_and_display
     rotate z by 70
     rotate y by 30
@@ -165,14 +132,29 @@ proc visualize_apt {} {
 
 proc visualize_oda {} {
     delete_all_reps
-    visualize_residues 0 "resname ODA" 2.3
-    visualize_residues 1 "name N" 2.3
-    mol modcolor 1 top "ColorID 3"
+    visualize_residues 0 {resname ODA and name "C.*"} 2.3
+    mol modcolor 0 top "ColorID 2"
+    visualize_residues 1 {resname ODA and name "H.*"} 2.3
+    mol modcolor 1 top "ColorID 8"
+    visualize_residues 2 "name N" 2.3
+    mol modcolor 2 top "ColorID 3"
     reset_view_and_display
     rotate x by 20
 }
 
-load_molecule gro "npt.gro"
+set structure [lindex $argv 0]
+puts "Structure: $structure"
+
+proc load_molecule {type filename} {
+    if {[file exists $filename]} {
+        mol new $filename type $type
+    } else {
+        puts "Error: File $filename does not exist."
+    }
+}
+
+# Replace "your_file.gro" with your actual filename
+load_molecule gro $structure
 delete_all_reps
 visualize_system_face_cut
 render_image "structure_100Oda.png"
@@ -180,14 +162,14 @@ render_image "structure_100Oda.png"
 delete_all_reps
 mol delete all
 
-load_molecule gro "npt.gro"
+load_molecule gro $structure
 delete_all_reps
 visualize_np_core
 render_image "structure_core.png"
 
 mol delete all
 
-load_molecule gro "npt.gro"
+load_molecule gro $structure
 delete_all_reps
 visualize_np
 render_image "structure_np.png"
