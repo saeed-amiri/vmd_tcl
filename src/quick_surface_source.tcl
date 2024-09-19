c37e6e30161143a342def1f60a46ae0fb999ce33
Saeed Amiri <samiri@uni-bremen.de>
2024-09-19 11:22:34 +0200

    visualize the box

--- src/quick_surface_source.tcl
+++ src/quick_surface_source.tcl
@@ -101,7 +101,43 @@ proc visualize_system_np_cut {} {
 }
 
 
-proc rotate_box {} {
-    rotate_view 0 0 -65
-    rotate_view -85 0 0
+proc visualize_system_box {} {
+    # Selecting water molecules
+    visualize_residues 0 {resname SOL and z < 110 and (x+y+z)<320} .5
+    mol modstyle 0 0 QuickSurf 1.200000 0.500000 1.000000 1.000000
+    mol modcolor 0 0 ColorID 15
+    color change rgb 15 0.540000 0.850000 0.999000
+    mol modmaterial 0 0 Transparent
+    material change opacity Transparent 0.40000
+    display rendermode GLSL
+    display height 5.00
+    # Selecting core of the nanoparticle
+    visualize_residues 1 {resname COR} 2.0
+    ## Selecting aptes of the nanoparticle with the NH3 atoms hidden
+    visualize_residues 2 {resname APT and not name N} 2.0
+    mol modcolor 2 top "ColorID 2"
+    visualize_residues 3 {resname ODN and z>97 and name "C.*" } 3
+    mol modcolor 3 top "ColorID 2"
+    visualize_residues 4 {name N} 3.0
+    mol modcolor 4 top "ColorID 0"
+    ## Selecting the NH2 atoms of the ODA surfactants
+    visualize_residues 5 {resname ODA and name "H.*"} 2.3
+    mol modcolor 5 top "ColorID 8"
+    visualize_residues 6 "name NH2" 4.0
+    mol modcolor 6 top "ColorID 3"
+    visualize_residues 7 "name CLA" 3.0
+    mol modcolor 7 top "ColorID 7"
+    visualize_residues 8 "name POT" 3.0
+    mol modcolor 8 top "ColorID 30"
+    visualize_residues 9 "resname D10 and z > 90 and (x+y+z)<320" 0.50
+    mol modcolor 9 top "ColorID 4"
+    mol modstyle 9 top QuickSurf 1.200000 0.500000 1.000000 1.000000
+    mol modmaterial 9 top Transparent
+    material change opacity Transparent 0.40000
+
+    reset_view_and_display
+    display depthcue off
+    pbc box
+    pbc box -width 6 -color black
+    rotate_box
 }
