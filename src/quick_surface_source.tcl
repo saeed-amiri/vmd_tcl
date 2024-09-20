eb0c28a4930f5196efe3979db03568d5cb090e48
Saeed Amiri <samiri@uni-bremen.de>
2024-09-20 07:59:00 +0200

    proc for video

--- src/quick_surface_source.tcl
+++ src/quick_surface_source.tcl
@@ -186,3 +186,49 @@ proc visualize_edl_structure {} {
     pbc box -width 6 -color black
 
 }
+proc rotate_clip {} {
+    rotate_view 0 0 65
+    rotate_view -85 0 0
+}
+proc visualize_system_clip {} {
+    # Selecting water molecules
+    visualize_residues 0 {resname SOL and z < 125} .5
+    mol selupdate 0 0 1
+    mol modstyle 0 0 QuickSurf 1.200000 0.500000 1.000000 1.000000
+    mol modcolor 0 0 ColorID 15
+    color change rgb 15 0.540000 0.850000 0.999000
+    mol modmaterial 0 0 Transparent
+    material change opacity Transparent 0.30000
+    display rendermode GLSL
+    display height 5.00
+    # Selecting core of the nanoparticle
+    visualize_residues 1 {resname COR} 2.0
+    mol smoothrep 0 1 3
+    ## Selecting aptes of the nanoparticle with the NH3 atoms hidden
+    visualize_residues 2 {resname APT and not name N} 2.0
+    mol smoothrep 0 2 3
+    mol modcolor 2 top "ColorID 2"
+    visualize_residues 3 {resname ODN and z>97 and name "C.*" } 3
+    mol smoothrep 0 3 3
+    mol modcolor 3 top "ColorID 2"
+    visualize_residues 4 {name N} 3.0
+    mol smoothrep 0 4 3
+    mol modcolor 4 top "ColorID 0"
+    ## Selecting the NH2 atoms of the ODA surfactants
+    visualize_residues 5 {resname ODA and name "H.*"} 2.3
+    mol smoothrep 0 5 3
+    mol modcolor 5 top "ColorID 8"
+    visualize_residues 6 "name NH2" 4.0
+    mol smoothrep 0 6 3
+    mol modcolor 6 top "ColorID 3"
+    visualize_residues 7 "name CLA and z < 125" 3.0
+    mol smoothrep 0 7 3
+    mol modcolor 7 top "ColorID 7"
+    visualize_residues 8 "name POT and z < 125" 3.0
+    mol smoothrep 0 8 3
+    mol modcolor 8 top "ColorID 30"
+    reset_view_and_display
+    display depthcue off
+    color Display Background white
+
+}
