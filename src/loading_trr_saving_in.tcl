fd41be01b60ec74d07fbcba92f2cb8b13ad3b016
Saeed Amiri <samiri@uni-bremen.de>
2024-10-18 07:08:41 +0200

    update

--- src/loading_trr_saving_in.tcl
+++ src/loading_trr_saving_in.tcl
@@ -1,20 +1,20 @@
 # Load the gro and trr and with optinal number of frames
-set mol [mol new "/scratch/saeed/GÖHBP/PRE_DFG_4Mar2024/single_np/vmd_test/15Oda/npt.gro" type gro waitfor all]
+set mol [mol new "/scratch/saeed/GÖHBP/PRE_DFG_4Mar2024/single_np/vmd_test/50Oda/center_mol_unwrap.gro" type gro waitfor all]
 puts "Attempting to load file: $mol"
-set distance 10
+set distance 5
 
-mol addfile "/scratch/saeed/GÖHBP/PRE_DFG_4Mar2024/single_np/vmd_test/15Oda/npt.trr" type trr first 0 last 100 step 1 waitfor all molid $mol
+mol addfile "/scratch/saeed/GÖHBP/PRE_DFG_4Mar2024/single_np/vmd_test/50Oda/center_mol_unwrap.trr" type trr first 0 last 1 step 1 waitfor all molid $mol
 
 set nf [molinfo top get numframes]
 puts " The numbers of frames are $nf"
 for {set i 0} {$i < $nf} {incr i} {
     animate goto $i
-    set inRadiusResidues [atomselect top "within $distance of (resname APT or resname COR)"]
+    set inRadiusResidues [atomselect top "resname APT COR"]
     set resIDs [$inRadiusResidues get residue]
     if {[llength $resIDs] > 0} {
         set uniqueResIDs [lsort -unique $resIDs]
         set completeInRadiusResidues [atomselect top "residue $uniqueResIDs"]
-        $completeInRadiusResidues writegro "all_including_apt_cor_near_apt_cor_${i}.gro"
+        $completeInRadiusResidues writepdb "COR_${i}.pdb"
         $completeInRadiusResidues delete
     } else {
         puts "No residues within $distance Å of APT or COR in frame $i"
