585532d44d4e816de01b1346c272d055f82c4e30
Saeed Amiri <samiri@uni-bremen.de>
2024-11-07 07:58:53 +0100

    update to get the correct residues

--- src/atom_extract.tcl
+++ src/atom_extract.tcl
@@ -1,21 +1,23 @@
 
     # Load the gro and trr and with optinal number of frames
-    set mol [mol new ./center_npt.gro  type gro waitfor all]
+    set mol [mol new ./np_centered_whole.gro  type gro waitfor all]
     puts "Attempting to load file: $mol"
-    set distance 3.0
+    set distance 3.6
 
-    mol addfile ./center_npt.trr type trr first 0 last -1 step 1    waitfor all molid $mol
+    mol addfile ./np_centered_whole.trr type trr first 0 last -1 step 1    waitfor all molid $mol
 
     set nf [molinfo top get numframes]
     puts " The numbers of frames are $nf"
     for {set i 0} {$i < $nf} {incr i} {
         animate goto $i
-        set inRadiusResidues [atomselect top "within $distance of resname APT COR"]
+        # set inRadiusResidues [atomselect top "within $distance of resname APT COR"]
+        set inRadiusResidues [atomselect top "((within $distance of resname APT COR) or (resname D10 and z > 130 and z < 160)) and (not resname ODN)"]
+
         set resIDs [$inRadiusResidues get residue]
         if {[llength $resIDs] > 0} {
             set uniqueResIDs [lsort -unique $resIDs]
-            set completeInRadiusResidues            [atomselect top "residue $uniqueResIDs"]
-            $completeInRadiusResidues writegro "extract_frames/apt_cor_${i}.gro"
+            set completeInRadiusResidues  [atomselect top "residue $uniqueResIDs"]
+            $completeInRadiusResidues writegro "./apt_cor_${i}.gro"
             $completeInRadiusResidues delete
         } else {
             puts "No residues within $distance Å of APT or COR in frame $i"
@@ -24,5 +26,3 @@
     }
 
     exit
-
-    
